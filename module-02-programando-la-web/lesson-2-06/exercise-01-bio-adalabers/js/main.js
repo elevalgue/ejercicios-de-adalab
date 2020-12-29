cb8be5ef22ed865f43048313f87486c4b63ff142
elevalgue <elena.valenzuela.guerrero@gmail.com>
2020-12-29 10:18:48 +0100

    Repaso ejercicios objetos

---
+++ module-02-programando-la-web/lesson-2-06/exercise-01-bio-adalabers/js/main.js
@@ -0,0 +1,11 @@
+'use strict'; 
+
+const adalaber1 = {};
+adalaber1.name = "Elena";
+adalaber1.age = 32;
+adalaber1.job = "Marketer";
+
+
+console.log(
+    `Mi nombre es ${adalaber1.name}, tengo ${adalaber1.age} años y antes era ${adalaber1.job}.`
+);
