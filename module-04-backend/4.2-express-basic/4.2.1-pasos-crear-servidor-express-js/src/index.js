2d504a9b431e9c682b72b4de8f0be2f88d68f14b
elevalgue <elena.valenzuela.guerrero@gmail.com>
2021-01-21 19:42:38 +0100

    Finish steps

---
+++ module-04-backend/4.2-express-basic/4.2.1-pasos-crear-servidor-express-js/src/index.js
@@ -0,0 +1,19 @@
+// 1. Debemos crear un proyecto o repo.
+
+// 2. Debemos crear un package.json usando npm init o creándolo a mano.
+
+// 3. Debemos instalar el módulo express ejecutando en la terminal npm install express
+
+        // Para saber que lo hemos hecho bien, dentro del fichero package.json debe aparecer express dentro de dependencies
+// 4. Debemos instalar el módulo cors ejecutando en la terminal npm install cors.
+        // Para saber que lo hemos hecho bien, dentro del fichero package.json debe aparecer express dentro de dependencies
+        
+// 5. Debemos crear un fichero index.js dentro de la carpeta src donde programaremos el servidor.
+
+// 6. Para arrancar debemos hacer una de las siguientes cosas:
+        // Ejecutar el comando node src/index.js o...
+        // Añadir al package.json el script:
+
+                /*"scripts": {
+                "start": "node src/index.js"
+                }*/
