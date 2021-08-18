3f5eb9d470e6e96f6334bca7f54038d5aa1611f6
elena <elena.valenzuela.guerrero@gmail.com>
2021-08-18 16:49:41 +0200

    Add exercise 02

--- module-02-programando-la-web/lesson-2-04/exercise-02-funcion-media/main.js
+++ module-02-programando-la-web/lesson-2-04/exercise-02-funcion-media/main.js
@@ -1,20 +1,9 @@
 "use strict";
 
-function caculateMedia(a, b, c, d) {
+function getAverage(a, b, c, d) {
   return (a + b + c + d) / 4;
 }
 
 const average = (7, 8, 15, 8) / 4;
 console.log("La nota media de Paco en esta evaluación es", average);
 
-// function sum(a, b, c) {
-//   return a + b + c;
-// }
-
-// const amount = sum(3, 56, 12);
-// console.log("Cantidad", amount);
-
-// const totalAges = sum(35, 26, 30);
-// console.log("totalAges", totalAges);
-
-// console.log("Exercises completed", sum(2, 6, 9));
