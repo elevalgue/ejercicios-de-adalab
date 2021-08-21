ccf8e1664d91169306d383cf650e2316c1e47ab0
elena <elena.valenzuela.guerrero@gmail.com>
2021-08-21 22:14:38 +0200

    Add exercise 02

--- module-02-programando-la-web/lesson-2-09/exercise-02-sotiremun/main.js
+++ module-02-programando-la-web/lesson-2-09/exercise-02-sotiremun/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-console.log('Are you ready for a good time? :);
+console.log('Are you ready for a good time? :)');
+
+let numbers = [];
+
+function get100Numbers(arr) {
+  for (let index = 0; index < 100; index++) {
+    numbers.push(index++)
+  }
+}
+
+get100Numbers(numbers);
+console.log(numbers);
+
+function getReversed100Numbers(arr) {
+  get100Numbers();
+  arr.reverse()
+}
+
+getReversed100Numbers(numbers)
+console.log(numbers);
