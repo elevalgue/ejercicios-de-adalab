db9ba983b3b17b519286b2103f6cad378b80436d
elena <elena.valenzuela.guerrero@gmail.com>
2021-08-23 10:14:54 +0200

    Add exercise 05

---
+++ module-02-programando-la-web/lesson-2-09/exercise-05-bonus-crea-tu-arbol-de-navidad/main.js
@@ -0,0 +1,14 @@
+'use strict';
+console.log('Are you ready for a good time? :))');
+
+
+let tree = '';
+
+function displayTree(height) {
+  for (let index = 0; index < height; index++) {
+    tree += '▲'; 
+    console.log(tree);
+  }
+}
+
+displayTree(5);
