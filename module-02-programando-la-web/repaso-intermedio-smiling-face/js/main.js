22b0fbb1d8f28c742588f0fa031190d32f0e8e40
elena <elena.valenzuela.guerrero@gmail.com>
2021-08-24 18:20:59 +0200

    Clean styles

--- module-02-programando-la-web/repaso-intermedio-smiling-face/js/main.js
+++ module-02-programando-la-web/repaso-intermedio-smiling-face/js/main.js
@@ -8,13 +8,14 @@ const buttonEl = document.querySelector('#js-btn');
 const selectEl = document.querySelector('#js-select-face');
 const faceEl = document.querySelector('span'); 
 
-const getRandomNumber = () => parseInt(Math.floor(Math.random() * 100));
 
 function handleClick(ev) {
     ev.preventDefault(); 
     switchBackground();
     switchFace(); 
 }
+
+const getRandomNumber = () => parseInt(Math.floor(Math.random() * 100));
 
 function switchBackground() {
     console.log(getRandomNumber());
@@ -23,11 +24,9 @@ function switchBackground() {
 
     if (getRandomNumber() % 2 === 0) {
         bodyEl.classList.add('happy-face');
-        // bodyEl.classList.remove('sad-face');
         console.log('maricarmen');
     } else {
         bodyEl.classList.add('sad-face');
-        // bodyEl.classList.remove('happy-face');
         console.log('pepino');
     }
 }
