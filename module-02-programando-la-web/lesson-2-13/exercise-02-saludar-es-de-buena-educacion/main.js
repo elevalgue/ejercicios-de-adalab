9667b4f8b555deec48ecccdf001ba0c0d9cb84d5
elena <elena.valenzuela.guerrero@gmail.com>
2021-09-01 23:13:09 +0200

    Add exercise 02

---
+++ module-02-programando-la-web/lesson-2-13/exercise-02-saludar-es-de-buena-educacion/main.js
@@ -0,0 +1,8 @@
+'use strict';
+
+console.log('Are you ready for a good time? :))');
+
+const names = ['María', 'Lucía', 'Susana', 'Rocío', 'Inmaculada'];
+const greetings = names.map(name => `Nice to see you, ${name}`);
+
+console.log(greetings);
