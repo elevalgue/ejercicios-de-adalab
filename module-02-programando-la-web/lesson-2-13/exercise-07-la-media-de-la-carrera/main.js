0909a7fe8dc82e4eefefa89437c6bb696214ccdf
elena <elena.valenzuela.guerrero@gmail.com>
2021-09-02 12:43:50 +0200

    Add exercise 07

---
+++ module-02-programando-la-web/lesson-2-13/exercise-07-la-media-de-la-carrera/main.js
@@ -0,0 +1,8 @@
+'use strict';
+
+console.log('Are you ready for a good time? :))');
+
+const times = [56, 9, 45, 28, 35];
+const average = times.reduce((acc, number) => (acc + number) / times.length)
+
+console.log(average, 'media');
