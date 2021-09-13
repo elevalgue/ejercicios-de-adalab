dd3d7c90dd1583a382af03623af4d217b7d654aa
elena <elena.valenzuela.guerrero@gmail.com>
2021-09-13 18:50:57 +0200

    Add exercisess

---
+++ module-02-programando-la-web/lesson-2-10/exercise-07-pintando-varias-imagenes-a-la-vez/js/main.js
@@ -0,0 +1,44 @@
+const createPromise = () =>
+  fetch('https://dog.ceo/api/breeds/image/random')
+    .then(response => response.json());
+
+function getBreedImages () {
+  const promises = [
+    createPromise(),
+    createPromise(),
+    createPromise(),
+    createPromise(),
+    createPromise(),
+    createPromise(),
+    createPromise(),
+    createPromise(),
+    createPromise(),
+    createPromise(),
+    createPromise(),
+    createPromise(),
+    createPromise(),
+    createPromise(),
+    createPromise(),
+    createPromise(),
+    createPromise(),
+    createPromise(),
+    createPromise(),
+    createPromise(),
+    createPromise(),
+    createPromise(),
+    createPromise(),
+    createPromise(),
+    createPromise()];
+  console.log(promises, 'promesas');
+
+  Promise.all(promises)
+    .then(responses => {
+      for (let i = 0; i < responses.length; i++) {
+        const img = document.querySelector('.dog' + (i + 1));
+        img.src = responses[i].message;
+      }
+    });
+}
+
+const btn = document.querySelector('.js-btn');
+btn.addEventListener('click', getBreedImages);
