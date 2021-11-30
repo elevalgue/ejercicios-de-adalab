4db215410457c3fa2c831112cd0dfa79993d44e9
elena <elena.valenzuela.guerrero@gmail.com>
2021-11-30 19:14:11 +0100

    Add image without storing it in the project

---
+++ module-03-react/lesson-3-01/exercise-04-working-with-images-2/src/components/App.js
@@ -0,0 +1,12 @@
+import '../styles/App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="title">Hola mundo</h1>
+      <img src="https://beta.adalab.es/resources/images/adalab-logo-128x128.png" title="Página de Adalab" alt="Logo de Adalab" />
+    </div>
+  );
+}
+
+export default App;
