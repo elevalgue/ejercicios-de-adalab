d842345ebd171bfa13350ae3a0c0d01115100683
elena <elena.valenzuela.guerrero@gmail.com>
2021-11-30 19:20:14 +0100

    Add html & styles

---
+++ module-03-react/lesson-3-01/exercise-05-migrar-html-a-react/src/components/App.js
@@ -0,0 +1,22 @@
+import '../styles/App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <main className="main">
+      <div className="main-title">
+        <span>#CC212A</span>
+        <span>Fire red engine</span>
+      </div>
+    </main>
+    <footer className="footer">
+      <div className="wrapper">
+        <div className="footer-letter">R</div>
+        <span className="footer-copy">&copy; 2021 FER.</span>
+      </div>
+    </footer>
+    </div>
+  );
+}
+
+export default App;
