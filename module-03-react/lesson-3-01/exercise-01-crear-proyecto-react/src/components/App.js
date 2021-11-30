01e6f9482e7d0f25d79b58662d05235d57df9058
elena <elena.valenzuela.guerrero@gmail.com>
2021-11-30 18:25:27 +0100

    Add some styles

--- module-03-react/lesson-3-01/exercise-01-crear-proyecto-react/src/components/App.js
+++ module-03-react/lesson-3-01/exercise-01-crear-proyecto-react/src/components/App.js
@@ -2,14 +2,14 @@ import '../styles/App.css';
 
 function App() {
   return (
-  <body className="happy-face" id="js-body">
+    <body className="happy-face" id="js-body">
       <h1><span id="js-face" className="face">:)</span></h1>
       <form className="form">
-          <select name="select" id="js-select-face" className="select">
-            <option value=":)" >:)</option>
-            <option value=":(" >:(</option>
-          </select>
-          <button id="js-btn" className="btn">Update</button>
+        <select name="select" id="js-select-face" className="select">
+          <option value=":)" >:)</option>
+          <option value=":(" >:(</option>
+        </select>
+        <button id="js-btn" className="btn">Update</button>
       </form>
     </body>
   );
