d317d3bcf4eaa6ec7327a20c4557e41ec5442066
elena <elena.valenzuela.guerrero@gmail.com>
2021-11-30 19:08:36 +0100

    Add image

---
+++ module-03-react/lesson-3-01/exercise-03-working-with-images/src/components/App.js
@@ -0,0 +1,13 @@
+import '../styles/App.scss';
+import logo from '../images/logo.png'; 
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="title">Hola mundo</h1>
+      <img src={logo} title="Adalab" alt="Logo de Adalab"></img>
+    </div>
+  );
+}
+
+export default App;
