73dfcc94a8c20121b0f1839b472f6a2d0f036da3
elena <elena.valenzuela.guerrero@gmail.com>
2021-12-01 18:39:09 +0100

    Add reduct & reset functionalities

--- module-03-react/lesson-3-02/exercise-02-el-contador/src/components/App.js
+++ module-03-react/lesson-3-02/exercise-02-el-contador/src/components/App.js
@@ -3,15 +3,26 @@ import { useState } from 'react';
 
 function App() {
   const [counter, setCounter] = useState(0);
-  
-  const handleCounter = e => {
+
+  const handleIncreament = e => {
     setCounter(counter + 1); 
   }
+
+  const handleReduction = e => {
+    setCounter(counter - 1); 
+  }
+
+  const handleReset = e => {
+    setCounter(0); 
+  }
+
   return (
     <div className="App">
       <h1 className="title">Contador</h1>
       <p>Y vamos por... {counter}</p>
-      <button onClick={handleCounter}>Clica aquí que sube al contador</button>
+      <button onClick={handleIncreament}>➕ 1️</button>
+      <button onClick={handleReduction}>➖ 1️</button>
+      <button onClick={handleReset}>RESET</button>
     </div>
   );
 }
