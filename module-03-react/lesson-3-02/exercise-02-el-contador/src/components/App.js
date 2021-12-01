d1d6676966b86443550570d5512e386091cba513
elena <elena.valenzuela.guerrero@gmail.com>
2021-12-01 16:59:31 +0100

    Increase counter

--- module-03-react/lesson-3-02/exercise-02-el-contador/src/components/App.js
+++ module-03-react/lesson-3-02/exercise-02-el-contador/src/components/App.js
@@ -2,11 +2,16 @@ import '../styles/App.scss';
 import { useState } from 'react'; 
 
 function App() {
-  const [counter, setCounter] = useState(0); 
+  const [counter, setCounter] = useState(0);
+  
+  const handleCounter = e => {
+    setCounter(counter + 1); 
+  }
   return (
     <div className="App">
       <h1 className="title">Contador</h1>
-      <p>Contador: </p>
+      <p>Y vamos por... {counter}</p>
+      <button onClick={handleCounter}>Clica aquí que sube al contador</button>
     </div>
   );
 }
