b348e347c27248c9f04046a482804a157b4a39a1
elena <elena.valenzuela.guerrero@gmail.com>
2021-12-01 14:29:18 +0100

    Use emoji as a favicon

--- module-03-react/lesson-3-02/exercise-02-el-contador/src/components/App.js
+++ module-03-react/lesson-3-02/exercise-02-el-contador/src/components/App.js
@@ -1,9 +1,12 @@
 import '../styles/App.scss';
+import { useState } from 'react'; 
 
 function App() {
+  const [counter, setCounter] = useState(0); 
   return (
     <div className="App">
-      <h1 className="title">Hola mundo</h1>
+      <h1 className="title">Contador</h1>
+      <p>Contador: </p>
     </div>
   );
 }
