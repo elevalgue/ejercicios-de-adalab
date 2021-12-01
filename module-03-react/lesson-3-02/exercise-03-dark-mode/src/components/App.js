fcb5b96996f96f8a6f20d109dc38e8366496d3f0
elena <elena.valenzuela.guerrero@gmail.com>
2021-12-01 19:02:17 +0100

    Switch background-color

--- module-03-react/lesson-3-02/exercise-03-dark-mode/src/components/App.js
+++ module-03-react/lesson-3-02/exercise-03-dark-mode/src/components/App.js
@@ -1,9 +1,22 @@
 import '../styles/App.scss';
+import { useState } from 'react'; 
 
 function App() {
+  const [changeBackground, setChangeBackground] = useState();
+
+  const renderDarkModeBackground = () => {
+    setChangeBackground(!changeBackground)
+  }
+
   return (
-    <div className="App">
-      <h1 className="title">Hola mundo</h1>
+    <div className="App light-background">
+      <div className={changeBackground ? "dark-background": null}>
+        <h1 className="title">Hola mundo</h1>
+        <button type="button" onClick={renderDarkModeBackground}>Des / activar el dark mode</button>
+        <p>Tienes activado el dark mode</p>
+        <p>The best Lorem Ipsum Generator in all the sea! Heave this scurvy copyfiller fer yar next adventure and cajol yar clients into walking the plank with ev'ry layout! Configure above, then get yer pirate ipsum...own the high seas, arg!
+        </p>
+      </div>
     </div>
   );
 }
