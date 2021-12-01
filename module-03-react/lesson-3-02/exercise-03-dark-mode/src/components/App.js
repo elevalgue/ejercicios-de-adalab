0b95319e85ca29e26035ba7585de8489c50bc5f4
elena <elena.valenzuela.guerrero@gmail.com>
2021-12-01 19:24:32 +0100

    Add main color to the body

--- module-03-react/lesson-3-02/exercise-03-dark-mode/src/components/App.js
+++ module-03-react/lesson-3-02/exercise-03-dark-mode/src/components/App.js
@@ -5,16 +5,17 @@ function App() {
   const [changeBackground, setChangeBackground] = useState();
 
   const renderDarkModeBackground = () => {
+    // document.body.classList.add('light-background');
     setChangeBackground(!changeBackground)
   }
 
   return (
-    <div className="App light-background">
-      <div className={changeBackground ? "dark-background": null}>
+    <div className="App ">
+      <div className={changeBackground ? "dark-background": "light-background"}>
         <h1 className="title">Hola mundo</h1>
         <button type="button" onClick={renderDarkModeBackground}>Des / activar el dark mode</button>
         <p>Tienes activado el dark mode</p>
-        <p>The best Lorem Ipsum Generator in all the sea! Heave this scurvy copyfiller fer yar next adventure and cajol yar clients into walking the plank with ev'ry layout! Configure above, then get yer pirate ipsum...own the high seas, arg!
+        <p>The best Lorem Ipsum Generator in all the sea! Heave this scurvy copyfiller fer yar next adventure and cajol yar clients into walking the plank with ev'ry layout! Configure above, then get yer pirate ipsum...own the high seas, arg! The best Lorem Ipsum Generator in all the sea! Heave this scurvy copyfiller fer yar next adventure and cajol yar clients into walking the plank with ev'ry layout! Configure above, then get yer pirate ipsum...own the high seas, arg! The best Lorem Ipsum Generator in all the sea! Heave this scurvy copyfiller fer yar next adventure and cajol yar clients into walking the plank with ev'ry layout! Configure above, then get yer pirate ipsum...own the high seas, arg! The best Lorem Ipsum Generator in all the sea! Heave this scurvy copyfiller fer yar next adventure and cajol yar clients into walking the plank with ev'ry layout! Configure above, then get yer pirate ipsum...own the high seas, arg! The best Lorem Ipsum Generator in all the sea! Heave this scurvy copyfiller fer yar next adventure and cajol yar clients into walking the plank with ev'ry layout! Configure above, then get yer pirate ipsum...own the high seas, arg! The best Lorem Ipsum Generator in all the sea! Heave this scurvy copyfiller fer yar next adventure and cajol yar clients into walking the plank with ev'ry layout! Configure above, then get yer pirate ipsum...own the high seas, arg! The best Lorem Ipsum Generator in all the sea! Heave this scurvy copyfiller fer yar next adventure and cajol yar clients into walking the plank with ev'ry layout! Configure above, then get yer pirate ipsum...own the high seas, arg! The best Lorem Ipsum Generator in all the sea! Heave this scurvy copyfiller fer yar next adventure and cajol yar clients into walking the plank with ev'ry layout! Configure above, then get yer pirate ipsum...own the high seas, arg! The best Lorem Ipsum Generator in all the sea! Heave this scurvy copyfiller fer yar next adventure and cajol yar clients into walking the plank with ev'ry layout! Configure above, then get yer pirate ipsum...own the high seas, arg! The best Lorem Ipsum Generator in all the sea! Heave this scurvy copyfiller fer yar next adventure and cajol yar clients into walking the plank with ev'ry layout! Configure above, then get yer pirate ipsum...own the high seas, arg!
         </p>
       </div>
     </div>
