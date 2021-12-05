f259734ad62c2e0dbadd68d58a51f7586896c1d8
elena <elena.valenzuela.guerrero@gmail.com>
2021-12-05 12:43:05 +0100

    Add playground 00

---
+++ module-03-react/lesson-3-03/playground-00-forms/src/components/App.js
@@ -0,0 +1,15 @@
+import '../styles/App.scss';
+
+// Fichero src/components/App.js
+const App = () => {
+  return (
+    <div>
+      <form>
+        <label htmlFor="name">Escribe un nombre:</label>
+        <input type="name" name="name" id="name" placeholder="María García" />
+      </form>
+    </div>
+  );
+};
+
+export default App;
