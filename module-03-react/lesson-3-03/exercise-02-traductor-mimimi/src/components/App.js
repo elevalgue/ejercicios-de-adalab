ef339ef128d794716d3068721798880c3fafe01d
elena <elena.valenzuela.guerrero@gmail.com>
2021-12-11 19:16:28 +0100

    Clean

--- module-03-react/lesson-3-03/exercise-02-traductor-mimimi/src/components/App.js
+++ module-03-react/lesson-3-03/exercise-02-traductor-mimimi/src/components/App.js
@@ -1,31 +1,20 @@
 import '../styles/App.scss';
-// import { useState } from 'react';
-
-let vowel1 = 'a';
-let newVowel = vowel1.replace(vowel1, 'i' ); 
-console.log(newVowel, 'era una a')
-
-let vowel2 = 'e';
-let newVowel2 = vowel2.replace(vowel2, 'i' ); 
-console.log(newVowel2, 'era una e')
-
-let vowel3 = 'o';
-let newVowel3 = vowel3.replace(vowel3, 'i' ); 
-console.log(newVowel3, 'era una o');
-
-let vowel4 = 'u';
-let newVowel4 = vowel4.replace(vowel4, 'i' ); 
-console.log(newVowel4, 'era una u');
-
+import { useState } from 'react';
 
 function App() {
+  const [translation, setTranslation] = useState('');
+  
+  const handleTranslation = ev => {
+    const translation = ev.currentTarget.value;
+    return setTranslation(translation.replaceAll(/[aeiou]/g, 'i')); 
+  };
   return (
     <div className="App">
       <h1 className="title">Traductor MIMIMI</h1>
-      <p>{ }</p>
+      <p>{translation}</p>
       <form>
         <label htmlFor="">Escribe tu texto aquí</label>
-        <textarea id="" ></textarea>
+        <textarea id="" onChange={handleTranslation}></textarea>
       </form>
     </div>
   );
