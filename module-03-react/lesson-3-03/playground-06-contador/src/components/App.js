1223919aabc817e9fdc7e8b258795c38c80f8bb0
elena <elena.valenzuela.guerrero@gmail.com>
2021-12-11 19:50:22 +0100

    Add folders

---
+++ module-03-react/lesson-3-03/playground-06-contador/src/components/App.js
@@ -0,0 +1,40 @@
+import '../styles/App.scss';
+// Fichero src/components/App.js
+import { useState } from 'react';
+
+const App = () => {
+  const [counter, setCounter] = useState(0);
+
+  const handleCounterInput = ev => {
+    // Cambiamos el valor de counter
+    setCounter(ev.target.value);
+  };
+
+  const handleResetCounter = () => {
+    // Aquí también cambiamos el valor de counter
+    setCounter(0);
+  };
+
+  const handleSubmit = ev => {
+    ev.preventDefault();
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <h1>El contador:</h1>
+        <label htmlFor="counter">Escribe aquí el valor del contador:</label>
+        <input
+          type="number"
+          name="counter"
+          id="counter"
+          value={counter}
+          onChange={handleCounterInput} />
+        <button onClick={handleResetCounter}>Resetear el contador</button>
+        <p>Valor de contador: {counter}</p>
+      </form>
+    </div>
+  );
+};
+
+export default App;
