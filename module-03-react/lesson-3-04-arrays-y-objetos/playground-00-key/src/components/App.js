995debb50892115d809d482fe85eeefe898919ea
elena <elena.valenzuela.guerrero@gmail.com>
2022-01-27 10:46:43 +0100

    Add jey playground

---
+++ module-03-react/lesson-3-04-arrays-y-objetos/playground-00-key/src/components/App.js
@@ -0,0 +1,39 @@
+import '../styles/App.scss';
+
+
+const App = () => {
+  const adalabers = [
+    {
+      name: 'María',
+      id: 'a',
+    },
+  {
+      name: 'Sonia',
+      id: 'b',
+    },
+  {
+      name: 'Alicia',
+      id: 'c',
+    },];
+
+  const renderAdalabers = () => {
+    return adalabers.map((adalaber) => {
+      return (
+        <li key={adalaber.id}>
+          {/* Aquí podemos poner todas las etiquetas que queramos */}
+          <span className="adalaber__prefix">El nombre de la alumna es:</span>
+          <strong className="adalaber__name">{adalaber.name}</strong>
+        </li>
+      );
+    });
+  };
+
+  return (
+    <div>
+      <h1 className="title">Lista de Adalabers</h1>
+      <ul>{renderAdalabers()}</ul>
+    </div>
+  );
+};
+
+export default App;
