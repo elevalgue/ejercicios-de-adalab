2475db25e6732f6bf939c7c1d05783ebd0ed3b0d
elena <elena.valenzuela.guerrero@gmail.com>
2022-01-27 12:54:07 +0100

    Test with different states options

--- module-03-react/lesson-3-04-arrays-y-objetos/04-objetos/00-playground/src/components/App.js
+++ module-03-react/lesson-3-04-arrays-y-objetos/04-objetos/00-playground/src/components/App.js
@@ -1,11 +1,50 @@
 import '../styles/App.scss';
+import { useState } from 'react';
 
-function App() {
+const App = () => {
+  // Estados
+
+  // const [shipping, setShipping] = useState({});
+  // const [shippingCity, setShippingCity] = useState({});
+
+  const [shipping, setShipping] = useState({ address: '', city: '' });
+
+  // Eventos
+
+  const handleAddress = ev => {
+    shipping.maricarmen = ev.target.value;
+    setShipping( {...shipping} );
+  };
+
+  // const handleCity = ev => {
+  //   shippingCity.city = ev.target.value;
+  //   setShippingCity({ ...shipping });
+  // };
+
+  const handleCity = ev => {
+    shipping.city = ev.target.value;
+    setShipping({ ...shipping });
+  };
+
   return (
-    <div className="App">
-      <h1 className="title">Hola mundo</h1>
+    <div>
+      <h1>Indica tu dirección para el envío:</h1>
+
+      <form>
+        <label htmlFor="maricarmen">Indica tu dirección postal:</label>
+        <input type="text" id="maricarmen" value={shipping.maricarmen} onChange={handleAddress} />
+        <label htmlFor="city">Indica tu ciudad:</label>
+        {/* <input type="text" id="city" value={shippingCity.city} onChange={handleCity} /> */}
+        <input type="text" id="city" value={shipping.city} onChange={handleCity} />
+      </form>
+
+      <p>
+        Te lo vamos a enviar a la dirección <strong>{shipping.maricarmen}</strong> en la ciudad
+        {/* <strong> {shippingCity.city}</strong>. */}
+        <strong> {shipping.city}</strong>.
+      </p>
     </div>
   );
-}
+};
 
 export default App;
