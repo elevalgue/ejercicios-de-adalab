a8790e5a8d89771baf4d35ed5eeb876cd477ffb6
elena <elena.valenzuela.guerrero@gmail.com>
2022-01-27 20:40:58 +0100

    Add fetch by name search

--- module-03-react/lesson-3-05-fetch-y-local-storage/02-fetch/src/components/App.js
+++ module-03-react/lesson-3-05-fetch-y-local-storage/02-fetch/src/components/App.js
@@ -1,21 +1,36 @@
 import '../styles/App.scss';
 
 // Fichero src/components/App.js
-// importamos useEffect además de useState
 import { useEffect, useState } from 'react';
+import callToApi from '../services/api'; // Importamos el servicio que acabamos de crear
 
 const App = () => {
+  // Estados
+
+  const [starWarsData, setStarWarsData] = useState({});
+
+  // Llamar al api con useEffect
+
   useEffect(() => {
-    fetch('https://swapi.dev/api/people/5')
-      .then(response => response.json())
-      .then(responseData => {
-        setStarWarsData(responseData);
-      });
+    // Dentro de useEffect llamamos al API
+    callToApi().then(response => {
+      // Cuando el API responde guardamos los datos en el estado para que se re-renderice el componente
+      setStarWarsData(response);
+    });
+    // Aquí ponemos un array vacío porque queremos que se llame al API solo la primera vez
   }, []);
 
   return (
     <div>
-      ...
+      <h1>Llamar al API de Star Wars</h1>
+      <h2>Características de {starWarsData.name}</h2>
+
+      <ul>
+        <li>Año de nacimiento: {starWarsData.birthYear}</li>
+        <li>Estatura: {starWarsData.height} cm</li>
+        <li>Peso: {starWarsData.mass} Kg</li>
+        <li>Color de ojos: {starWarsData.eyeColor}</li>
+      </ul>
     </div>
   );
 };
