2a90b9db547f4ce121ee3c3e1143b28a23680435
elena <elena.valenzuela.guerrero@gmail.com>
2022-04-04 21:49:13 +0200

    Add favicon

--- module-03-react/hangman-game/src/components/App.js
+++ module-03-react/hangman-game/src/components/App.js
@@ -1,11 +1,67 @@
 import '../styles/App.scss';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <h1 className="title">Hola mundo</h1>
+    <div className='page'>
+      <main className='main'>
+        <section>
+          <div className='solution'>
+            <h2 className='title'>Solución:</h2>
+            <ul className='letters'>
+              <li className='letter'>k</li>
+              <li className='letter'>a</li>
+              <li className='letter'></li>
+              <li className='letter'>a</li>
+              <li className='letter'>k</li>
+              <li className='letter'>r</li>
+              <li className='letter'></li>
+              <li className='letter'>k</li>
+              <li className='letter'>e</li>
+              <li className='letter'>r</li>
+            </ul>
+          </div>
+          <div className='error'>
+            <h2 className='title'>Letras falladas:</h2>
+            <ul className='letters'>
+              <li className='letter'>f</li>
+              <li className='letter'>q</li>
+              <li className='letter'>h</li>
+              <li className='letter'>p</li>
+              <li className='letter'>x</li>
+            </ul>
+          </div>
+          <form className='form'>
+            <label className='title' for='last-letter'>
+              Escribe una letra:
+            </label>
+            <input
+              autocomplete='off'
+              className='form__input'
+              maxlength='1'
+              type='text'
+              name='last-letter'
+              id='last-letter'
+            />
+          </form>
+        </section>
+        <section className='dummy error-5'>
+          <span className='error-13 eye'></span>
+          <span className='error-12 eye'></span>
+          <span className='error-11 line'></span>
+          <span className='error-10 line'></span>
+          <span className='error-9 line'></span>
+          <span className='error-8 line'></span>
+          <span className='error-7 line'></span>
+          <span className='error-6 head'></span>
+          <span className='error-5 line'></span>
+          <span className='error-4 line'></span>
+          <span className='error-3 line'></span>
+          <span className='error-2 line'></span>
+          <span className='error-1 line'></span>
+        </section>
+      </main>
     </div>
   );
-}
+};
 
 export default App;
--- module-03-react/hangman-game/src/index.js
+++ module-03-react/hangman-game/src/index.js
@@ -1,5 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './components/App';
+import Header from './components/Header';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <HashRouter>
+    <Header />
+    <App />
+  </HashRouter>,
+  document.getElementById('root')
+);
